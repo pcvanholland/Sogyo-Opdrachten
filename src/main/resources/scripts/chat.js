857ba426d3a76850a272ad0153a3b7b1f16027e5
C. van Holland <cvholland@sogyo.nl>
2020-01-27 11:35:40 +0100

    Moved folder to reduce clicks. Seperated JS from HTML.

---
+++ src/main/resources/scripts/chat.js
@@ -0,0 +1,20 @@
+const socket = new WebSocket("ws://localhost:9090");
+socket.onmessage = message => {
+    const newElement = document.createElement("div");
+    newElement.innerHTML = `<p>${message.data}</p>`;
+
+    document
+        .getElementById("messages")
+        .appendChild(newElement);
+};
+
+const sendMessage = function() {
+    const form = document.getElementById("sendMessageForm");
+    const text = form.elements["text"].value;
+
+    socket.send(text);
+
+    document
+        .getElementById("sendMessageForm")
+        .reset();
+}
