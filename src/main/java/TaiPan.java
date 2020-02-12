015897b39ee189d7d8a74be884076e8bb6eeb845
C. van Holland <cvholland@sogyo.nl>
2020-02-12 16:10:17 +0100

    Init project, made UML.

---
+++ src/main/java/TaiPan.java
@@ -0,0 +1,9 @@
+package taipan.domain;
+
+public class TaiPan
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Called main :)");
+    }
+}
---
+++ src/test/java/TaiPan_Test.java
@@ -0,0 +1,13 @@
+package taipan.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaiPan_Test
+{
+    @Test
+    public void test_init()
+    {
+        System.out.println("Called test :)");
+    }
+}
